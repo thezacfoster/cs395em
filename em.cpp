2b852108d9a6f748fea07348134d6d0934eb8d12
Zac Foster <43626969+thezacfoster@users.noreply.github.com>
2020-02-02 00:18:57 -0500

    actually added .cpp file (previously added incorrectly)

---
+++ em.cpp
@@ -0,0 +1,117 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+// Wait for given input before continueing
+void WaitFor(string message) {
+	while (true) {
+		string i;
+		 getline(cin, i);
+			if (i == message) {
+				break;
+			}
+	}
+}
+
+// Send a command to Camelot.
+void Action(string command) {
+	cout << ("start " + command) << endl;
+	WaitFor("succeeded " + command);
+}
+
+int main() {
+
+	// Set up a small house with a door.
+	Action("CreatePlace(BobsHouse, Cottage)");
+	Action("CreateCharacter(Bob, B)");
+	Action("SetClothing(Bob, Peasant)");
+	Action("SetHairStyle(Bob, Short_Full)");
+	Action("SetPosition(Bob, BobsHouse.Door)");
+	Action("CreateItem(HouseKey, BlueKey)");
+	Action("SetPosition(HouseKey, BobsHouse.Shelf)");
+	Action("EnableIcon(\"Pickup_Key\", Hand, BobsHouse.Shelf, \"Pick up key\", true)");
+	Action("EnableIcon(\"Open_Door\", Open, BobsHouse.Door, \"Leave the house\", true)");
+	Action("ShowMenu()");
+
+	vector<string> playerInv;
+
+	// Respond to input.
+	while (true) {
+		string i;
+		getline(cin, i);
+		if (i == "input Selected Start") {
+			Action("SetCameraFocus(Bob)");
+			Action("HideMenu()");
+			Action("EnableInput()");
+		}
+		else if (i == "input \"Pickup_Key\" BobsHouse.Shelf") {
+			playerInv.push_back("HouseKey");
+			Action("DisableIcon(\"Pickup_Key\", BobsHouse.Shelf)");
+			Action("SetPosition(HouseKey)");
+		}
+		else if (i == "input Key Inventory") {
+			for (string item : playerInv) {
+				Action("AddToList(" + item + ")");
+				//action('DisableInput()')
+				Action("ShowList(Bob)");
+			}
+		}
+		else if (i == "input Close List") {
+			Action("HideList()");
+			Action("ClearList()");
+		}
+		else if (i == "input \"Open_Door\" BobsHouse.Door") {
+			bool hasKey = false;
+			for (string item : playerInv) {
+				if (item == "HouseKey") {
+					hasKey = true;
+				}
+			}
+			if (hasKey == true) {
+				Action("Exit(Bob, BobsHouse.Door, true)");
+				Action("CreatePlace(Farm, Farm)");
+				Action("Enter(Bob, Farm.Door, true)");
+			}
+			else {
+				Action("SetNarration(\"The door is locked!\")");
+				Action("ShowNarration()");
+			}
+		}
+		else if (i == "input arrived Bob position Farm.Exit") {
+			//Action("Exit(Bob, Farm.Exit, true)");
+			Action("FadeOut()");
+			Action("DisableInput()");
+			Action("CreatePlace(Forest, ForestPath)");
+			Action("Enter(Bob, Forest.EastEnd, true)");
+			Action("EnableInput()");
+		}
+		else if (i == "input arrived Bob position Forest.WestEnd") {
+			//Action("Exit(Bob, Forest.WestEnd, true)");
+			Action("FadeOut()");
+			Action("DisableInput()");
+			Action("CreatePlace(City, City)");
+			Action("Enter(Bob, City.EastEnd, true)");
+			Action("EnableInput()");
+		}
+		else if (i == "input Key Pause") {
+			Action("DisableInput()");
+			Action("ShowMenu()");
+		}
+		else if (i == "input Selected Resume") {
+			Action("SetCameraFocus(Bob)");
+			Action("HideMenu()");
+			Action("EnableInput()");
+		}
+		else if (i == "input Close Narration") {
+			Action("HideNarration()");
+		}
+		else if (i == "input Selected Quit") {
+			Action("Quit()");
+		}
+	}
+
+	return 0;
+}
+
